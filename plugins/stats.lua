7706e0c5569b35e04be9762015e7445043260295
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-01-27 19:40:17 +0330

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -1,26 +1,4 @@
--- Saves the number of messages from a user
--- Can check the number of messages with !stats 
-
 do
-
-local NUM_MSG_MAX = 5
-local TIME_CHECK = 4 -- seconds
-
-local function user_print_name(user)
-  if user.print_name then
-    return user.print_name
-  end
-
-  local text = ''
-  if user.first_name then
-    text = user.last_name..' '
-  end
-  if user.lastname then
-    text = text..user.last_name
-  end
-
-  return text
-end
 
 -- Returns a table with `name` and `msgs`
 local function get_msgs_user_chat(user_id, chat_id)
@@ -29,11 +7,40 @@ local function get_msgs_user_chat(user_id, chat_id)
   local user = redis:hgetall(uhash)
   local um_hash = 'msgs:'..user_id..':'..chat_id
   user_info.msgs = tonumber(redis:get(um_hash) or 0)
-  user_info.name = user_print_name(user)..' ('..user_id..')'
+  user_info.name = user_print_name(user)..' ['..user_id..']'
   return user_info
 end
 
 local function chat_stats(chat_id)
+  -- Users on chat
+  local hash = 'chat:'..chat_id..':users'
+  local users = redis:smembers(hash)
+  local users_info = {}
+  -- Get user info
+  for i = 1, #users do
+    local user_id = users[i]
+    local user_info = get_msgs_user_chat(user_id, chat_id)
+    table.insert(users_info, user_info)
+  end
+  -- Sort users by msgs number
+  table.sort(users_info, function(a, b) 
+      if a.msgs and b.msgs then
+        return a.msgs > b.msgs
+      end
+    end)
+  local text = 'users in this chat \n'
+  for k,user in pairs(users_info) do
+    text = text..user.name..' = '..user.msgs..'\n'
+  end
+  local file = io.open("./groups/lists/"..chat_id.."stats.txt", "w")
+  file:write(text)
+  file:flush()
+  file:close() 
+  send_document("chat#id"..chat_id,"./groups/lists/"..chat_id.."stats.txt", ok_cb, false)
+  return --text
+end
+
+local function chat_stats2(chat_id)
   -- Users on chat
   local hash = 'chat:'..chat_id..':users'
   local users = redis:smembers(hash)
@@ -53,62 +60,13 @@ local function chat_stats(chat_id)
       end
     end)
 
-  local text = ''
+  local text = 'users in this chat \n'
   for k,user in pairs(users_info) do
-    text = text..'> '..user.name..' PMs:'..user.msgs..'\n'
+    text = text..user.name..' = '..user.msgs..'\n'
   end
-
   return text
 end
-
 -- Save stats, ban user
-local function pre_process(msg)
-  -- Ignore service msg
-  if msg.service then
-    print('Service message')
-    return msg
-  end
-
-  -- Save user on Redis
-  if msg.from.type == 'user' then
-    local hash = 'user:'..msg.from.id
-    print('Saving user', hash)
-    if msg.from.print_name then
-      redis:hset(hash, 'print_name', msg.from.print_name)
-    end
-    if msg.from.first_name then
-      redis:hset(hash, 'first_name', msg.from.first_name)
-    end
-    if msg.from.last_name then
-      redis:hset(hash, 'last_name', msg.from.last_name)
-    end
-  end
-
-  -- Save stats on Redis
-  if msg.to.type == 'chat' then
-    -- User is on chat
-    local hash = 'chat:'..msg.to.id..':users'
-    redis:sadd(hash, msg.from.id)
-  end
-
-  -- Total user msgs
-  local hash = 'msgs:'..msg.from.id..':'..msg.to.id
-  redis:incr(hash)
-
-  -- Check flood
-  if msg.from.type == 'user' then
-    local hash = 'user:'..msg.from.id..':msgs'
-    local msgs = tonumber(redis:get(hash) or 0)
-    if msgs > NUM_MSG_MAX then
-      print('User '..msg.from.id..'is flooding '..msgs)
-      msg = nil
-    end
-    redis:setex(hash, TIME_CHECK, msgs+1)
-  end
-
-  return msg
-end
-
 local function bot_stats()
 
   local redis_scan = [[
@@ -130,89 +88,63 @@ local function bot_stats()
   hash = 'chat:*:users'
   r = redis:eval(redis_scan, 1, hash)
   text = text..'\nGroups: '..r
-
   return text
-
 end
-
 local function run(msg, matches)
+  if matches[1]:lower() == 'teleseed' then -- Put everything you like :)
+    local about = _config.about_text
+    local name = user_print_name(msg.from)
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
+    return about
+  end 
+  if matches[1]:lower() == "statslist" then
+    if not is_momod(msg) then
+      return "For mods only !"
+    end
+    local chat_id = msg.to.id
+    local name = user_print_name(msg.from)
+    savelog(msg.to.id, name.." ["..msg.from.id.."] requested group stats ")
+    return chat_stats2(chat_id)
+  end
   if matches[1]:lower() == "stats" then
-
     if not matches[2] then
-      if msg.to.type == 'gp' then
+      if not is_momod(msg) then
+        return "For mods only !"
+      end
+      if msg.to.type == 'chat' then
         local chat_id = msg.to.id
+        local name = user_print_name(msg.from)
+        savelog(msg.to.id, name.." ["..msg.from.id.."] requested group stats ")
         return chat_stats(chat_id)
       else
-        return 'Only work in group'
+        return
       end
     end
-
-    if matches[2] == "bot" then
-      if not is_sudo(msg) then
-        return "You are NOT SUDO!!"
+    if matches[2] == "teleseed" then -- Put everything you like :)
+      if not is_admin(msg) then
+        return "For admins only !"
       else
         return bot_stats()
       end
     end
-
-    if matches[2] == "gp" then
-      if not is_sudo(msg) then
-        return "You are NOT GLOBAL ADMIN!"
+    if matches[2] == "group" then
+      if not is_admin(msg) then
+        return "For admins only !"
       else
         return chat_stats(matches[3])
       end
     end
   end
 end
-
 return {
-  description = "Robot, Groups and Member Stats", 
-  usage = {
-    "/stats : number of messages stats",
-    "/stats gp (id) : other group stats",
-    "/stats bot : robot stats"
-  },
   patterns = {
-    "^[!/](stats)$",
-    "^[!/](stats) (gp) (%d+)",
-    "^[!/](stats) (bot)"
+    "^[!/]([Ss]tats)$",
+    "^[!/]([Ss]tatslist)$",
+    "^[!/]([Ss]tats) (group) (%d+)",
+    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
+		"^[!/]([Tt]eleseed)"-- Put everything you like :)
     }, 
-  run = run,
-  pre_process = pre_process
+  run = run
 }
 
 end
-
-
-
-
---gps
-do
-
-function run(msg, matches)
-  local lat = matches[1]
-  local lon = matches[2]
-  local receiver = get_receiver(msg)
-
-  local zooms = {16, 18}
-  local urls = {}
-  for i = 1, #zooms do
-    local zoom = zooms[i]
-    local url = "http://maps.googleapis.com/maps/api/staticmap?zoom=" .. zoom .. "&size=600x300&maptype=roadmap&center=" .. lat .. "," .. lon .. "&markers=color:blue%7Clabel:X%7C" .. lat .. "," .. lon
-    table.insert(urls, url)
-  end
-
-  send_photos_from_url(receiver, urls)
-
-  return "www.google.es/maps/place/@" .. lat .. "," .. lon
-end
-
-return {
-  description = "Generate Map for GPS Coordinates", 
-  usage = "/gps (latitude,longitude) : generate map by gps cods",
-  patterns = {"^[!/]gps ([^,]*)[,%s]([^,]*)$"}, 
-  run = run 
-}
-
-end
-
