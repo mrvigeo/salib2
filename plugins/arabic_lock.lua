12d373e557838404f9430b181ede8fd1f4b4afe8
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-01-27 21:21:44 +0330

    Update arabic_lock.lua

--- plugins/arabic_lock.lua
+++ plugins/arabic_lock.lua
@@ -1,59 +1,6 @@
 
 antiarabic = {}-- An empty table for solving multiple kicking problem
 
-local function run(msg)
-if msg.text == "سلام" then
-	return "سلام خوبی؟"
-end
-if msg.text == "Hi" then
-	return "سلام ، فارسی تایپ کن"
-end
-if msg.text == "hi" then
-	return "سلام...  اگه میشه فارسی تایپ کن"
-end
-if msg.text == "hello" then
-	return "Hi honey"
-end
-if msg.text == "Salam" then
-	return "سلام علیکم ، فارسی تایپ کن"
-end
-if msg.text == "salam" then
-	return "و علیکومو سلام ، فارسی تایپ کن لطفا"
-end
-if msg.text == "خسته شدم" then
-	return "بس که دلم دنبال یک بهونه گشت"
-end
-if msg.text == "باشه" then
-	return "تکون نخور لاشه"
-end
-if msg.text == "باوشه" then
-	return "بشین چشات وا شه"
-end
-if msg.text == "ربات" then
-	return "بله ؟"
-end
-if msg.text == "کسکش" then
-	return "فحش نده"
-end
-if msg.text == "کونی" then
-	return "فحش نده"
-end
-if msg.text == "حروم زاده" then
-	return "مودب باش"
-end
-if msg.text == "?" then
-	return "بله ؟"
-end
-if msg.text == "Bye" then
-	return "بای عشقم"
-end
-if msg.text == "بای" then
-	return "خدافظ ، مواظب زیباییت باش"
-end
-if msg.text == "گه نخور" then
-        return "تو بخور"
-end
-
 do
 local function run(msg, matches)
   if is_momod(msg) then -- Ignore mods,owner,admins
