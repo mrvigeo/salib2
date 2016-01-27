9ca63828bf080678459eab9cc7aed6fdd7034614
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-01-27 19:29:55 +0330

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -1,31 +1,362 @@
 do
 
-local function send_title(cb_extra, success, result)
-  if success then
-    send_msg(cb_extra[1], cb_extra[2], ok_cb, false)
+-- Check Member
+local function check_member_autorealm(cb_extra, success, result)
+  local receiver = cb_extra.receiver
+  local data = cb_extra.data
+  local msg = cb_extra.msg
+  for k,v in pairs(result.members) do
+    local member_id = v.id
+    if member_id ~= our_id then
+      -- Group configuration
+      data[tostring(msg.to.id)] = {
+        group_type = 'Realm',
+        settings = {
+          set_name = string.gsub(msg.to.print_name, '_', ' '),
+          lock_name = 'yes',
+          lock_photo = 'no',
+          lock_member = 'no',
+          flood = 'yes'
+        }
+      }
+      save_data(_config.moderation.data, data)
+      local realms = 'realms'
+      if not data[tostring(realms)] then
+        data[tostring(realms)] = {}
+        save_data(_config.moderation.data, data)
+      end
+      data[tostring(realms)][tostring(msg.to.id)] = msg.to.id
+      save_data(_config.moderation.data, data)
+      return send_large_msg(receiver, 'Welcome to your new realm !')
+    end
+  end
+end
+local function check_member_realm_add(cb_extra, success, result)
+  local receiver = cb_extra.receiver
+  local data = cb_extra.data
+  local msg = cb_extra.msg
+  for k,v in pairs(result.members) do
+    local member_id = v.id
+    if member_id ~= our_id then
+      -- Group configuration
+      data[tostring(msg.to.id)] = {
+        group_type = 'Realm',
+        settings = {
+          set_name = string.gsub(msg.to.print_name, '_', ' '),
+          lock_name = 'yes',
+          lock_photo = 'no',
+          lock_member = 'no',
+          flood = 'yes'
+        }
+      }
+      save_data(_config.moderation.data, data)
+      local realms = 'realms'
+      if not data[tostring(realms)] then
+        data[tostring(realms)] = {}
+        save_data(_config.moderation.data, data)
+      end
+      data[tostring(realms)][tostring(msg.to.id)] = msg.to.id
+      save_data(_config.moderation.data, data)
+      return send_large_msg(receiver, 'Realm has been added!')
+    end
+  end
+end
+function check_member_group(cb_extra, success, result)
+  local receiver = cb_extra.receiver
+  local data = cb_extra.data
+  local msg = cb_extra.msg
+  for k,v in pairs(result.members) do
+    local member_id = v.id
+    if member_id ~= our_id then
+      -- Group configuration
+      data[tostring(msg.to.id)] = {
+        group_type = 'Group',
+        moderators = {},
+        set_owner = member_id ,
+        settings = {
+          set_name = string.gsub(msg.to.print_name, '_', ' '),
+          lock_name = 'yes',
+          lock_photo = 'no',
+          lock_member = 'no',
+          flood = 'yes',
+        }
+      }
+      save_data(_config.moderation.data, data)
+      local groups = 'groups'
+      if not data[tostring(groups)] then
+        data[tostring(groups)] = {}
+        save_data(_config.moderation.data, data)
+      end
+      data[tostring(groups)][tostring(msg.to.id)] = msg.to.id
+      save_data(_config.moderation.data, data)
+      return send_large_msg(receiver, 'You have been promoted as the owner.')
+    end
+  end
+end
+local function check_member_modadd(cb_extra, success, result)
+  local receiver = cb_extra.receiver
+  local data = cb_extra.data
+  local msg = cb_extra.msg
+  for k,v in pairs(result.members) do
+    local member_id = v.id
+    if member_id ~= our_id then
+      -- Group configuration
+      data[tostring(msg.to.id)] = {
+        group_type = 'Group',
+        moderators = {},
+        set_owner = member_id ,
+        settings = {
+          set_name = string.gsub(msg.to.print_name, '_', ' '),
+          lock_name = 'yes',
+          lock_photo = 'no',
+          lock_member = 'no',
+          flood = 'yes',
+        }
+      }
+      save_data(_config.moderation.data, data)
+      local groups = 'groups'
+      if not data[tostring(groups)] then
+        data[tostring(groups)] = {}
+        save_data(_config.moderation.data, data)
+      end
+      data[tostring(groups)][tostring(msg.to.id)] = msg.to.id
+      save_data(_config.moderation.data, data)
+      return send_large_msg(receiver, 'Group is added and you have been promoted as the owner ')
+    end
+  end
+end
+local function automodadd(msg)
+  local data = load_data(_config.moderation.data)
+  if msg.action.type == 'chat_created' then
+    receiver = get_receiver(msg)
+    chat_info(receiver, check_member_group,{receiver=receiver, data=data, msg = msg})
+  end
+end
+local function autorealmadd(msg)
+  local data = load_data(_config.moderation.data)
+  if msg.action.type == 'chat_created' then
+    receiver = get_receiver(msg)
+    chat_info(receiver, check_member_autorealm,{receiver=receiver, data=data, msg = msg})
+  end
+end
+local function check_member_realmrem(cb_extra, success, result)
+  local receiver = cb_extra.receiver
+  local data = cb_extra.data
+  local msg = cb_extra.msg
+  for k,v in pairs(result.members) do
+    local member_id = v.id
+    if member_id ~= our_id then
+      -- Realm configuration removal
+      data[tostring(msg.to.id)] = nil
+      save_data(_config.moderation.data, data)
+      local realms = 'realms'
+      if not data[tostring(realms)] then
+        data[tostring(realms)] = nil
+        save_data(_config.moderation.data, data)
+      end
+      data[tostring(realms)][tostring(msg.to.id)] = nil
+      save_data(_config.moderation.data, data)
+      return send_large_msg(receiver, 'Realm has been removed!')
+    end
+  end
+end
+local function check_member_modrem(cb_extra, success, result)
+  local receiver = cb_extra.receiver
+  local data = cb_extra.data
+  local msg = cb_extra.msg
+  for k,v in pairs(result.members) do
+    local member_id = v.id
+    if member_id ~= our_id then
+      -- Group configuration removal
+      data[tostring(msg.to.id)] = nil
+      save_data(_config.moderation.data, data)
+      local groups = 'groups'
+      if not data[tostring(groups)] then
+        data[tostring(groups)] = nil
+        save_data(_config.moderation.data, data)
+      end
+      data[tostring(groups)][tostring(msg.to.id)] = nil
+      save_data(_config.moderation.data, data)
+      return send_large_msg(receiver, 'Group has been removed')
+    end
+  end
+end
+--End Check Member
+local function show_group_settingsmod(msg, data, target)
+ 	if not is_momod(msg) then
+    	return "For moderators only!"
+  	end
+  	local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+     	if data[tostring(msg.to.id)]['settings']['flood_msg_max'] then
+        	NUM_MSG_MAX = tonumber(data[tostring(msg.to.id)]['settings']['flood_msg_max'])
+        	print('custom'..NUM_MSG_MAX)
+      	else 
+        	NUM_MSG_MAX = 5
+      	end
+    end
+    local bots_protection = "Yes"
+    if data[tostring(msg.to.id)]['settings']['lock_bots'] then
+    	bots_protection = data[tostring(msg.to.id)]['settings']['lock_bots']
+   	end
+    local leave_ban = "no"
+    if data[tostring(msg.to.id)]['settings']['leave_ban'] then
+    	leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
+   	end
+  local settings = data[tostring(target)]['settings']
+  local text = "Group settings:\nLock group name : "..settings.lock_name.."\nLock group photo : "..settings.lock_photo.."\nLock group member : "..settings.lock_member.."\nLock group leave : "..leave_ban.."\nflood sensitivity : "..NUM_MSG_MAX.."\nBot protection : "..bots_protection--"\nPublic: "..public
+  return text
+end
+
+local function set_descriptionmod(msg, data, target, about)
+  if not is_momod(msg) then
+    return "For moderators only!"
+  end
+  local data_cat = 'description'
+  data[tostring(target)][data_cat] = about
+  save_data(_config.moderation.data, data)
+  return 'Set group description to:\n'..about
+end
+local function get_description(msg, data)
+  local data_cat = 'description'
+  if not data[tostring(msg.to.id)][data_cat] then
+    return 'No description available.'
+  end
+  local about = data[tostring(msg.to.id)][data_cat]
+  local about = string.gsub(msg.to.print_name, "_", " ")..':\n\n'..about
+  return 'About '..about
+end
+local function lock_group_arabic(msg, data, target)
+  if not is_momod(msg) then
+    return "For moderators only!"
+  end
+  local group_arabic_lock = data[tostring(target)]['settings']['lock_arabic']
+  if group_arabic_lock == 'yes' then
+    return 'Arabic is already locked'
+  else
+    data[tostring(target)]['settings']['lock_arabic'] = 'yes'
+    save_data(_config.moderation.data, data)
+    return 'Arabic has been locked'
   end
 end
 
-local function run(msg, matches)
-  local eq = URL.escape(matches[1])
-
-  local url = "http://latex.codecogs.com/png.download?"
-    .."\\dpi{300}%20\\LARGE%20"..eq
-
-  local receiver = get_receiver(msg)
-  local title = "Edit LaTeX on www.codecogs.com/eqnedit.php?latex="..eq
-  send_photo_from_url(receiver, url, send_title, {receiver, title})
+local function unlock_group_arabic(msg, data, target)
+  if not is_momod(msg) then
+    return "For moderators only!"
+  end
+  local group_arabic_lock = data[tostring(target)]['settings']['lock_arabic']
+  if group_arabic_lock == 'no' then
+    return 'Arabic is already unlocked'
+  else
+    data[tostring(target)]['settings']['lock_arabic'] = 'no'
+    save_data(_config.moderation.data, data)
+    return 'Arabic has been unlocked'
+  end
 end
 
-return {
-  description = "Convert Text to Image",
-  usage = {
-    "/conv (txt) : convert txt to img"
-  },
-  patterns = {
-    "^[!/]conv (.+)$"
-  },
-  run = run
-}
-
+local function lock_group_bots(msg, data, target)
+  if not is_momod(msg) then
+    return "For moderators only!"
+  end
+  local group_bots_lock = data[tostring(target)]['settings']['lock_bots']
+  if group_bots_lock == 'yes' then
+    return 'Bots protection is already enabled'
+  else
+    data[tostring(target)]['settings']['lock_bots'] = 'yes'
+    save_data(_config.moderation.data, data)
+    return 'Bots protection has been enabled'
+  end
 end
+
+local function unlock_group_bots(msg, data, target)
+  if not is_momod(msg) then
+    return "For moderators only!"
+  end
+  local group_bots_lock = data[tostring(target)]['settings']['lock_bots']
+  if group_bots_lock == 'no' then
+    return 'Bots protection is already disabled'
+  else
+    data[tostring(target)]['settings']['lock_bots'] = 'no'
+    save_data(_config.moderation.data, data)
+    return 'Bots protection has been disabled'
+  end
+end
+
+local function lock_group_namemod(msg, data, target)
+  if not is_momod(msg) then
+    return "For moderators only!"
+  end
+  local group_name_set = data[tostring(target)]['settings']['set_name']
+  local group_name_lock = data[tostring(target)]['settings']['lock_name']
+  if group_name_lock == 'yes' then
+    return 'Group name is already locked'
+  else
+    data[tostring(target)]['settings']['lock_name'] = 'yes'
+    save_data(_config.moderation.data, data)
+    rename_chat('chat#id'..target, group_name_set, ok_cb, false)
+    return 'Group name has been locked'
+  end
+end
+local function unlock_group_namemod(msg, data, target)
+  if not is_momod(msg) then
+    return "For moderators only!"
+  end
+  local group_name_set = data[tostring(target)]['settings']['set_name']
+  local group_name_lock = data[tostring(target)]['settings']['lock_name']
+  if group_name_lock == 'no' then
+    return 'Group name is already unlocked'
+  else
+    data[tostring(target)]['settings']['lock_name'] = 'no'
+    save_data(_config.moderation.data, data)
+    return 'Group name has been unlocked'
+  end
+end
+local function lock_group_floodmod(msg, data, target)
+  if not is_owner(msg) then
+    return "Only admins can do it for now"
+  end
+  local group_flood_lock = data[tostring(target)]['settings']['flood']
+  if group_flood_lock == 'yes' then
+    return 'Group flood is locked'
+  else
+    data[tostring(target)]['settings']['flood'] = 'yes'
+    save_data(_config.moderation.data, data)
+    return 'Group flood has been locked'
+  end
+end
+
+local function unlock_group_floodmod(msg, data, target)
+  if not is_owner(msg) then
+    return "Only admins can do it for now"
+  end
+  local group_flood_lock = data[tostring(target)]['settings']['flood']
+  if group_flood_lock == 'no' then
+    return 'Group flood is not locked'
+  else
+    data[tostring(target)]['settings']['flood'] = 'no'
+    save_data(_config.moderation.data, data)
+    return 'Group flood has been unlocked'
+  end
+end
+
+local function lock_group_membermod(msg, data, target)
+  if not is_momod(msg) then
+    return "For moderators only!"
+  end
+  local group_member_lock = data[tostring(target)]['settings']['lock_member']
+  if group_member_lock == 'yes' then
+    return 'Group members are already locked'
+  else
+    data[tostring(target)]['settings']['lock_member'] = 'yes'
+    save_data(_config.moderation.data, data)
+  end
+  return 'Group members has been locked'
+end
+
+local function unlock_group_membermod(msg, data, target)
+  if not is_momod(msg) then
+    return "For moderators only!"
+  end
+  local group_member_lock = data[tostring(target)]['settings']['lock_member']
+  if group_member_lock == 'no' then
+    return 'Group members are not locke
