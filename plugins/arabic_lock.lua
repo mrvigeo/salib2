b2e45eda8f8d58f418274b4e932d3c366f9a8e58
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-01-27 20:36:41 +0330

    Update arabic_lock.lua

--- plugins/arabic_lock.lua
+++ plugins/arabic_lock.lua
@@ -1,43 +1,6 @@
 
 antiarabic = {}-- An empty table for solving multiple kicking problem
 
-do
-local function run(msg, matches)
-  if is_momod(msg) then -- Ignore mods,owner,admins
-    return
-  end
-  local data = load_data(_config.moderation.data)
-  if data[tostring(msg.to.id)]['settings']['lock_arabic'] then
-    if data[tostring(msg.to.id)]['settings']['lock_arabic'] == 'yes' then
-      if antiarabic[msg.from.id] == true then 
-        return
-      end
-      send_large_msg("chat#id".. msg.to.id , "Arabic is not allowed here")
-      local name = user_print_name(msg.from)
-      savelog(msg.to.id, name.." ["..msg.from.id.."] kicked (arabic was locked) ")
-      chat_del_user('chat#id'..msg.to.id,'user#id'..msg.from.id,ok_cb,false)
-		  antiarabic[msg.from.id] = true
-      return
-    end
-  end
-  return
-end
-local function cron()
-  antiarabic = {} -- Clear antiarabic table 
-end
-return {
-  patterns = {
-    "([\216-\219][\128-\191])"
-    },
-  run = run,
-	cron = cron
-}
-
-
-
-
-
-
 local function run(msg)
 if msg.text == "سلام" then
 	return "سلام خوبی؟"
@@ -90,24 +53,37 @@ end
 if msg.text == "گه نخور" then
         return "تو بخور"
 end
-end
 
+do
+local function run(msg, matches)
+  if is_momod(msg) then -- Ignore mods,owner,admins
+    return
+  end
+  local data = load_data(_config.moderation.data)
+  if data[tostring(msg.to.id)]['settings']['lock_arabic'] then
+    if data[tostring(msg.to.id)]['settings']['lock_arabic'] == 'yes' then
+      if antiarabic[msg.from.id] == true then 
+        return
+      end
+      send_large_msg("chat#id".. msg.to.id , "Arabic is not allowed here")
+      local name = user_print_name(msg.from)
+      savelog(msg.to.id, name.." ["..msg.from.id.."] kicked (arabic was locked) ")
+      chat_del_user('chat#id'..msg.to.id,'user#id'..msg.from.id,ok_cb,false)
+		  antiarabic[msg.from.id] = true
+      return
+    end
+  end
+  return
+end
+local function cron()
+  antiarabic = {} -- Clear antiarabic table 
+end
 return {
-	description = "Chat With Robot Server", 
-	usage = "chat with robot",
-	patterns = {
-		"^[Hh]i$",
-		"^[Hh]ello$",
-		"^[Zz]ac$",
-		"^ZAC$",
-		"^[Bb]ot$",
-		"^[Uu]mbrella$",
-		"^[Bb]ye$",
-		"^?$",
-		"^[Ss]alam$",
-		}, 
-	run = run,
-    --privileged = true,
-	pre_process = pre_process
+  patterns = {
+    "([\216-\219][\128-\191])"
+    },
+  run = run,
+	cron = cron
 }
+
 end
