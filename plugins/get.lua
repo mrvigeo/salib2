ce67cb4591bfef68ba35507a022d80c67fefc40a
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-01-27 19:29:09 +0330

    Update get.lua

--- plugins/get.lua
+++ plugins/get.lua
@@ -2,124 +2,36 @@ local function get_variables_hash(msg)
   if msg.to.type == 'chat' then
     return 'chat:'..msg.to.id..':variables'
   end
-  if msg.to.type == 'user' then
-    return 'user:'..msg.from.id..':variables'
-  end
 end 
-
-local function list_variables(msg)
-  local hash = get_variables_hash(msg)
-  
-  if hash then
-    local names = redis:hkeys(hash)
-    local text = ''
-    for i=1, #names do
-      text = '> '..text..names[i]..'\n'
-    end
-    return text
-  end
-end
 
 local function get_value(msg, var_name)
   local hash = get_variables_hash(msg)
   if hash then
     local value = redis:hget(hash, var_name)
     if not value then
-      return'Not found variable'
+      return
     else
-      return value
+      return var_name..' :\n'..value
     end
   end
 end
 
 local function run(msg, matches)
+  if not is_momod(msg) then -- only for mods,owner and admins
+    return 
+  end
   if matches[2] then
+    local name = user_print_name(msg.from)
+    savelog(msg.to.id, name.." ["..msg.from.id.."] used /get ".. matches[2])-- save to logs
     return get_value(msg, matches[2])
   else
-    return list_variables(msg)
+    return
   end
 end
 
 return {
-  description = "View Saved Variables With !set", 
-  usage = {
-	"/get : view all variables",
-	"/get (value) : view variable",
-	},
   patterns = {
-    "^([!/]get) (.+)$",
-    "^[!/]get$"
+    "^([!/]get) (.+)$"
   },
   run = run
 }
-
-
-
-
-
---map
-
-
-
--- Implement a command !loc [area] which uses
--- the static map API to get a location image
-
--- Not sure if this is the proper way
--- Intent: get_latlong is in time.lua, we need it here
--- loadfile "time.lua"
-
--- Globals
--- If you have a google api key for the geocoding/timezone api
-do
-
-local api_key = nil
-
-local base_api = "https://maps.googleapis.com/maps/api"
-
-function get_staticmap(area)
-  local api        = base_api .. "/staticmap?"
-
-  -- Get a sense of scale
-  local lat,lng,acc,types = get_latlong(area)
-
-  local scale = types[1]
-  if     scale=="locality" then zoom=8
-  elseif scale=="country"  then zoom=4
-  else zoom = 13 end
-    
-  local parameters =
-    "size=600x300" ..
-    "&zoom="  .. zoom ..
-    "&center=" .. URL.escape(area) ..
-    "&markers=color:red"..URL.escape("|"..area)
-
-  if api_key ~=nil and api_key ~= "" then
-    parameters = parameters .. "&key="..api_key
-  end
-  return lat, lng, api..parameters
-end
-
-
-function run(msg, matches)
-  local receiver	= get_receiver(msg)
-  local lat,lng,url	= get_staticmap(matches[1])
-
-  -- Send the actual location, is a google maps link
-  send_location(receiver, lat, lng, ok_cb, false)
-
-  -- Send a picture of the map, which takes scale into account
-  send_photo_from_url(receiver, url)
-
-  -- Return a link to the google maps stuff is now not needed anymore
-  return nil
-end
-
-return {
-  description = "Get Man Location by Name", 
-  usage = "/map (name) : get map and location",
-  patterns = {"^[!/]map (.*)$"}, 
-  run = run 
-}
-
-end
-
