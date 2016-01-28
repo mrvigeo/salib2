75ec400adba0a914095e7972537abcb7818dbebf
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-01-28 18:39:44 +0330

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -1,9 +1,9 @@
 local function run(msg)
-if msg.text == "سلام" then
-	return "سلام خوبی؟"
+if msg.text == "ARJ" then
+	return " جونم ؟ چی شده؟"
 end
-if msg.text == "Hi" then
-	return "سلام ، فارسی تایپ کن"
+if msg.text == "A.R.J" then
+	return "بله ، کاری داری ؟"
 end
 if msg.text == "hi" then
 	return "سلام...  اگه میشه فارسی تایپ کن"
@@ -17,37 +17,34 @@ end
 if msg.text == "salam" then
 	return "و علیکومو سلام ، فارسی تایپ کن لطفا"
 end
-if msg.text == "خسته شدم" then
-	return "بس که دلم دنبال یک بهونه گشت"
-end
-if msg.text == "باشه" then
+if msg.text == "bashe" then
 	return "تکون نخور لاشه"
 end
-if msg.text == "باوشه" then
+if msg.text == "Bashe" then
 	return "بشین چشات وا شه"
 end
-if msg.text == "ربات" then
+if msg.text == "bot" then
 	return "بله ؟"
 end
-if msg.text == "کسکش" then
+if msg.text == "sik :D" then
+	return " مودب باش رباتِ بی تلبیت"
+end
+if msg.text == "kooni" then
 	return "فحش نده"
 end
-if msg.text == "کونی" then
-	return "فحش نده"
-end
-if msg.text == "حروم زاده" then
+if msg.text == "suck it" then
 	return "مودب باش"
 end
-if msg.text == "?" then
+if msg.text == "؟" then
 	return "بله ؟"
 end
-if msg.text == "Bye" then
+if msg.text == "bye" then
 	return "بای عشقم"
 end
-if msg.text == "بای" then
+if msg.text == "Bye" then
 	return "خدافظ ، مواظب زیباییت باش"
 end
-if msg.text == "گه نخور" then
+if msg.text == "goh nakhor" then
         return "تو بخور"
 end
 end
