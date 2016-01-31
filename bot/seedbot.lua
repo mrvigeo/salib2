50b4e01304fa247787006bc22f9167ad42d3ad3f
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-01-31 22:47:33 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -225,7 +225,10 @@ function create_config( )
     "plugins",
     "txt2img",
     "dictionary",
-    "nas"
+    "nas",
+    "location",
+    "boobs",
+    "9gag"
     },
     sudo_users = {45446970,127646076,155942482,136378403},--Sudo users
     disabled_channels = {},
