b2203a2dd89fa7ef0b43d1de4aa8a019af4ca1d7
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-01 07:40:24 +0330

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -208,12 +208,12 @@ return {
           "!plugins reload : reloads all plugins." },
           },
   patterns = {
-    "^!plugins$",
-    "^!plugins? (enable) ([%w_%.%-]+)$",
-    "^!plugins? (disable) ([%w_%.%-]+)$",
-    "^!plugins? (enable) ([%w_%.%-]+) (chat)",
-    "^!plugins? (disable) ([%w_%.%-]+) (chat)",
-    "^!plugins? (reload)$" },
+    "^[/!]plugins$",
+    "^[/!]plugins? (enable) ([%w_%.%-]+)$",
+    "^[/!]plugins? (disable) ([%w_%.%-]+)$",
+    "^[/!]plugins? (enable) ([%w_%.%-]+) (chat)",
+    "^[/!]plugins? (disable) ([%w_%.%-]+) (chat)",
+    "^[/!]plugins? (reload)$" },
   run = run,
   moderated = true, -- set to moderator mode
   --privileged = true
