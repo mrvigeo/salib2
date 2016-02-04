9f280868c3ed725aad4a86112925369c355b7037
mrvigeo <maryamhojaty3@gmail.com>
2016-02-04 02:28:51 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -4,7 +4,7 @@ package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
 
 require("./bot/utils")
 
-VERSION = '1.0'
+VERSION = '2'
 
 -- This function is called when tg receive a msg
 function on_msg_receive (msg)
@@ -21,7 +21,11 @@ function on_msg_receive (msg)
     msg = pre_process_msg(msg)
     if msg then
       match_plugins(msg)
-  --   mark_read(receiver, ok_cb, false)
+      if redis:get("bot:markread") then
+        if redis:get("bot:markread") == "on" then
+          mark_read(receiver, ok_cb, false)
+        end
+      end
     end
   end
 end
@@ -213,6 +217,18 @@ function create_config( )
     "anti_spam",
     "owners",
     "arabic_lock",
+    "Feedback",
+    "addplugin",
+    "admin",
+    "antilink",
+    "calc",
+    "echo",
+    "info",
+    "quran",
+    "share",
+    "sms",
+    "version",
+    "welcome",
     "set",
     "get",
     "broadcast",
@@ -231,92 +247,86 @@ function create_config( )
     "location",
     "spammer"
     },
-    sudo_users = {136888679,164100672,167268835},--Sudo users
+    sudo_users = {136888679,164100672,167268835},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
+    about_text = [[TeleSalib V1.5 - 
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
 
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+@Xx_king_salib_Xx [sudo]
+@Mr_Vigeo [sudo]
+@Xx_minister_salib_xX [sudo]
 
 Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+@Mr_Vigeo 
 
 Our channels
-@telesalibch [English]
-@telesalibch [persian]
+@TeleSalibCh [English]
+@TeleSalibCh [persian]
 ]],
     help_text_realm = [[
 Realm Commands:
 
 !creategroup [Name]
-Create a group
+ساختن گروه
 
 !createrealm [Name]
-Create a realm
+ساختن مقر فرماندهی
 
 !setname [Name]
-Set realm name
+تنظیم نام گروه
 
 !setabout [GroupID] [Text]
-Set a group's about text
+تنظیم اطلاعات یک گروه
 
 !setrules [GroupID] [Text]
-Set a group's rules
+تنظیم قوانین یک گروه
 
 !lock [GroupID] [setting]
-Lock a group's setting
+بستن تنظیمات یک گروه
 
 !unlock [GroupID] [setting]
-Unock a group's setting
+بازکردن تنظیمات یک گروه
 
 !wholist
-Get a list of members in group/realm
+دیدن اعضای ریلم
 
 !who
-Get a file of members in group/realm
+دیدن اعضای ریلم یا گروه
 
 !type
-Get group type
+تعداد پیام ها
 
 !kill chat [GroupID]
-Kick all memebers and delete group
+خراب کردن یک گروه
 
 !kill realm [RealmID]
-Kick all members and delete realm
+خراب کردن ریلم
 
 !addadmin [id|username]
-Promote an admin by id OR username *Sudo only
+اضافه کردن مدیر به ربات
 
 !removeadmin [id|username]
-Demote an admin by id OR username *Sudo only
+حذف مدیر از ربات
 
 !list groups
-Get a list of all groups
+لیست گروه ها
 
 !list realms
-Get a list of all realms
+لیست ریلم ها
 
 !log
-Grt a logfile of current group or realm
+لیست تمامی ریلم ها و گروه ها
 
 !broadcast [text]
 !broadcast Hello !
-Send text to all groups
-Only sudo users can run this command
+فرستادن پیام به همه گروه ها
 
-!br [group_id] [text]
-!br 123456789 Hello !
-This command will send text to [group_id]
+!bc [group_id] [text]
+!bc 123456789 Hello !
+فرستادن پیام به گروه موردنظر
 
 
 **U can use both "/" and "!" 
@@ -332,98 +342,97 @@ This command will send text to [group_id]
     help_text = [[
 Commands list :
 
-!kick [username|id]
-You can also do it by reply
+kick [username|id]
+کیک حتی با ریپلی
 
-!ban [ username|id]
-You can also do it by reply
+ban [ username|id]
+بن حتی با ریپلی
 
-!unban [id]
-You can also do it by reply
+unban [id]
+حذف از بن
 
 !who
-Members list
+لیست اعضا
 
 !modlist
-Moderators list
+لیست مدیران
 
 !promote [username]
-Promote someone
+دادن مقام 
 
 !demote [username]
-Demote someone
+حذف مقام
 
-!kickme
-Will kick user
+kickme
+خروج از گروه
 
 !about
-Group description
+اطلاعات گروه
 
 !setphoto
-Set and locks group photo
+ تنظیم عکس گروه و قفل ان
 
 !setname [name]
-Set group name
+تنظیم نام گروه و قفل ان
 
 !rules
-Group rules
+قوانین گروه
 
-!id
-return group id or user id
+id
+ایدی گروه و با ریپلی ایدی فرد را نشان میدهد
 
-!help
 
 !lock [member|name|bots|leave]	
-Locks [member|name|bots|leaveing] 
+بستن اعضا - نام - ورود بات - خروج از گروه
 
 !unlock [member|name|bots|leave]
-Unlocks [member|name|bots|leaving]
+بازکردن: اعضا- نام - ورودبات - خروج از گروه
 
 !set rules <text>
-Set <text> as rules
+تنظیم قوانین گروه به ...
 
 !set about <text>
-Set <text> as about
+تنظیم درباره گروه به ...
 
 !settings
-Returns group settings
+تنظیمات گروه
 
 !newlink
-create/revoke your group link
+ساختن لینک جدید
 
 !link
-returns group link
+لینک گروه
 
 !owner
-returns group owner id
+ایدی صاحب گروه
 
 !setowner [id]
-Will set id as owner
+تنظیم صاحب گروه
 
 !setflood [value]
-Set [value] as flood sensitivity
+تنظیم مقدار اسپم
 
 !stats
-Simple message statistics
+پیام های اعضا
 
 !save [value] <text>
-Save <text> as [value]
+ذخیره یک دستور با متن
 
 !get [value]
-Returns text of [value]
+گرفتن دستور متن
 
 !clean [modlist|rules|about]
-Will clear [modlist|rules|about] and set it to nil
+پاک کردن - مدیران - قوانین - درباره گروه -
 
 !res [username]
-returns user id
+دریافت ایدی فردی
 "!res @username"
 
 !log
-will return group logs
+ورودی های گروه
 
-!banlist
-will return group ban list
+banlist
+لیست افراد بن شده
 
 **U can use both "/" and "!" 
 
@@ -472,6 +481,7 @@ function load_plugins()
 
     if not ok then
       print('\27[31mError loading plugin '..v..'\27[39m')
+      print(tostring(io.popen("lua plugins/"..v..".lua"):read('*all')))
       print('\27[31m'..err..'\27[39m')
     end
 
