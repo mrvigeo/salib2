40060e6b982d6c3412fc63917ffd1be999d635d9
mrvigeo <maryamhojaty3@gmail.com>
2016-02-04 01:13:16 -0800

    Create calc.lua

---
+++ plugins/calc.lua
@@ -0,0 +1,31 @@
+— Function reference: http://mathjs.org/docs/reference/functions/categorical.html
+
+local function mathjs(exp)
+  local url = 'http://api.mathjs.org/v1/'
+  url = url..'?expr='..URL.escape(exp)
+  local b,c = http.request(url)
+  local text = nil
+  if c == 200 then
+    text = 'جواب محسابه: '..b
+  
+  elseif c == 400 then
+    text = b
+  else
+    text = 'Unexpected error\n'
+      ..'Is api.mathjs.org up?'
+  end
+  return text
+end
+
+local function run(msg, matches)
+  return mathjs(matches[1])
+end
+
+return {
+  description = "Calculate math expressions with mathjs API",
+  usage = "calc [number]: جواب تمرین شما",
+  patterns = {
+    "^calc (.*)$"
+  },
+  run = run
+}
